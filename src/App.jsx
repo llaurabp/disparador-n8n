f529f7dc2a34b8699d00254bc1398b9259543cf7
Laura Borges <alaurabp@hotmail.com>
2025-04-08 13:56:16 -0300

    adicionando rota de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Disparo from './pages/Disparo';
+import LoginPage from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Disparo />} />
-        {/* Você pode adicionar mais rotas aqui futuramente */}
+        <Route path="/disparo" element={<Disparo />} />
+        <Route path="/" element={<LoginPage />} />
       </Routes>
     </Router>
   );
